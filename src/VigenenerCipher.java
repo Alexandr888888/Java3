fc855f2dc6058406d8a95b3f41831d41387d7467
Alexandr888888 <108408240+Alexandr888888@users.noreply.github.com>
2022-07-01 09:36:08 +0300

    Could not find or load main class Vigenener

--- src/VigenenerCipher.java
+++ src/VigenenerCipher.java
@@ -1,19 +1,22 @@
-public class Vigenener {
+public class VigenenerCipher {
+    final class Vigenener {
 
 
-    //public static void main(String [] args) {
+   // public static void main(String[] args) {
 
-        final class Vigenener {
+        //final class Vigenener {
             private final int bias,
                     letters;
+
 
             public Vigenener(final int bias, final int letters) {
                 this.bias = bias;
                 this.letters = letters;
             }
 
+
             public String encrypt(final String text, final String key) {
-                String encrypt = "hello";
+                String encrypt = "";
                 final int keyLen = key.length();
                 for (int i = 0, len = text.length(); i < len; i++) {
                     encrypt += (char) (((text.charAt(i) + key.charAt(i % keyLen) - 2 * this.bias) % this.bias) % this.letters) + this.bias;
@@ -23,24 +26,26 @@ public class Vigenener {
             }
 
 
-            final Vigenener v = new Vigenener(97, 26);
-            String enc = v.encrypt("english", key);
-
-
-            //public static void main(String[] args) {
-
-
+        //public static void main(String[] args){
+        //final String key = "en";
+        //final Vigenener v = new Vigenener(97, 26);
+        //String enc = v.encrypt("english", key);
+    //}
             public String decrypt(final String cipher, final String key) {
-                String decrypt = "";
-                final int keyLen = key.length();
-                for (int i = 0, len = cipher.length(); i < len; i++) {
-                    decrypt += (char) ((cipher.charAt(i) - key.charAt(i % keyLen) + this.letters) % this.letters) + this.bias;
-                }
-                return decrypt;
-            }
-            }
+            String decrypt = "";
+
+            final int keyLen = key.length();
+            for (int i = 0, len = cipher.length(); i < len; i++) {
+            decrypt += (char) ((cipher.charAt(i) - key.charAt(i % keyLen) + this.letters) % this.letters) + this.bias;
+        }
+        return decrypt;
+    }
 
 
+
+
+
+             //public static void main(String[] args) {
             final String key = "en";
             Vigenener v = new Vigenener(97, 26);
             final String enc = v.encrypt("english", key);
@@ -48,7 +53,7 @@ public class Vigenener {
             {
 
                 System.out.println(enc);
-                String dec = v.decrypt(enc,key);
+                String dec = v.decrypt(enc, key);
                 System.out.println(dec);
                 System.out.println((int) 'a');
                 System.out.println((int) 'z');
@@ -57,3 +62,4 @@ public class Vigenener {
         }
 
 
+    }
