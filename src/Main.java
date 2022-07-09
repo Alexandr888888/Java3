50fb33a6d8a82992da0145415092be5303acd536
Alexandr888888 <108408240+Alexandr888888@users.noreply.github.com>
2022-07-09 12:31:45 +0300

    finish code

--- src/Main.java
+++ src/Main.java
@@ -4,58 +4,30 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static void main(String[] args) {
-        menu();
-    }
-
-
-       public static String getRandomString ( int length){
-
-
-           String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-           Random random = new Random();
-           StringBuffer sb = new StringBuffer();
-           for (int i = 0; i < length; i++) {
-               int number = random.nextInt(52);
-               sb.append(str.charAt(number));
-           }
-
-
-           return sb.toString();
-       }
-
-
-        public  void caesar () {
-
+        public static void main(String[] args) {
+             menu();
         }
-
-        String text ="BOMB";
-        String cipher = Caesar.encrypt(text, 5);
-        {
+        public static String getRandomString ( int length){
 
 
-            System.out.println(cipher);
-        }
-         //public void caesarDecode () {
-
-
-        String getDecode = "Anafy";
-        String decode = CaesarDecode.decrypt(text, 5);
-        {
-            System.out.println(decode);
-        }
-        public void vigener () {
-            String cipher = "";
-           // if(Character.isUpperCase(intShift))
-         //    String text2 = vigener.encrypt(text,5);
-            {
-                System.out.println(cipher);
+            String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Random random = new Random();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < length; i++) {
+                int number = random.nextInt(52);
+                sb.append(str.charAt(number));
             }
 
-        }
-        static void menu() {
-            Scanner in = new Scanner(System.in);
 
+            return sb.toString();
+        }
+
+
+        static void menu () {
+
+            System.out.println("Введите слово:");
+            Scanner in = new Scanner(System.in);
+            String randomWord = in.nextLine();
             System.out.println("Menu:");
             System.out.println("1) Encrypt caesar");
             System.out.println("2) Decrypt caesar");
@@ -65,28 +37,35 @@ public class Main {
 
             switch (in.nextInt()) {
                 case 1:
-                    String cipher = "Vivat";
+                    String sim = getRandomString(52);
+
+                    if (Character.isLetter(sim.charAt(1))) ;
+                    String cipher = randomWord;
                     System.out.println(Caesar.encrypt(cipher, 5));
+
                     menu();
                     break;
                 case 2:
-                    String encryptMessage = ("Anafy");
+                    String decode = getRandomString(52);
+                    if (Character.isLetter(decode.charAt(1))) ;
+                    String encryptMessage = randomWord;
                     System.out.println(CaesarDecode.decrypt(encryptMessage, 5));
                     menu();
                     break;
                 case 3:
-                    String vigener = "Vivat";
+                    String code = getRandomString(52);
+                    if (Character.isLetter(code.charAt(1))) ;
+                    String vigener = randomWord;
                     char shift = 'G';
                     int intShift = (int) shift - 65;
-                    if (Character.isUpperCase(intShift))
-                        shift = (char) (int) (intShift);
+
                     System.out.println(Caesar.encrypt(vigener, intShift));
                     menu();
                     break;
-                default:
 
-                case 4:
-                    String encryptVigenerMessage = ("Bobgz");
+                case 4: String vigenerDecode = getRandomString(52);
+                    if (Character.isLetter(vigenerDecode.charAt(1))) ;
+                    String encryptVigenerMessage = randomWord;
                     char decryptShift = 'G';
 
                     int intDecryptShift = (int) decryptShift - 65;
@@ -96,20 +75,8 @@ public class Main {
                     menu();
 
                     break;
-            }
+                default:
+            }    }
         }
-          //public static String getRandomString ( int length){
 
-
-               // String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-               // Random random = new Random();
-               // StringBuffer sb = new StringBuffer();
-               // for (int i = 0; i < length; i++) {
-                    //int number = random.nextInt(52);
-                  // sb.append(str.charAt(number));
-               // }
-
-
-              //  return sb.toString();
-            }
 
